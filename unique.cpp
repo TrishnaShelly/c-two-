a5588dceee5d5406709dbf20cab5f49f718eb99b
TrishnaShelly <105371850+TrishnaShelly@users.noreply.github.com>
2022-09-15 21:46:38 +0530

    unique element in array

---
+++ unique.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int unique(int arr[], int n)
+{
+    int xorsum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        xorsum = xorsum ^ arr[i];
+    }
+    return xorsum;
+}
+
+int main()
+{
+    int arr[] = {2, 4, 4, 6, 6};
+    cout << unique(arr, 5);
+    return 0;
+}
