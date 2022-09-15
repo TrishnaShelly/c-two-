721f5a87574f4ebb3feaa5dc67653f295b278f6e
TrishnaShelly <105371850+TrishnaShelly@users.noreply.github.com>
2022-09-15 21:40:16 +0530

    Prime  Factorisation

--- sieve.cpp
+++ sieve.cpp
@@ -1,31 +1,59 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 void primefactor(int);
 
-void prime(int n){
-    int arr[100]={0};
-    for(int i=2;i<=n;i++){
-        if(arr[i]==0){
-            for(int j=i*i;j<=n;j+=i){
-                arr[j]=1;
+void prime(int n)
+{
+    int arr[100] = {0};
+    for (int i = 2; i <= n; i++)
+    {
+        if (arr[i] == 0)
+        {
+            for (int j = i * i; j <= n; j += i)
+            {
+                arr[j] = 1;
             }
         }
     }
-    for(int i=2;i<=n;i++){
-        if(arr[i]==0)
-        cout<<i<<" ";
+    for (int i = 2; i <= n; i++)
+    {
+        if (arr[i] == 0)
+            cout << i << " ";
     }
 }
 
-int main(){
+int main()
+{
     int n;
-    cin>>n;
-    prime(n);
+    cin >> n;
+    primefactor(n);
     return 0;
 }
 
-//prime factorisation using sieve 
-void primefactor(int n){
-
+// prime factorisation using sieve
+void primefactor(int n)
+{
+    int spf[100] = {0};
+    for (int i = 2; i <= n; i++){
+        spf[i] = i;
+    }
+        
+    for (int i = 2; i <= n; i++)
+    {
+        if (spf[i] == i)
+        {
+            for (int j = i * i; j <= n; j +=i)
+            {
+                if (spf[j] == j){
+                    spf[j] = i;
+                }
+                    
+            }
+        }
+    }
+    while(n>1){
+        cout<<spf[n]<<" ";
+        n=n/spf[n];
+    }
 }
