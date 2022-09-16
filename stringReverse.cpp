40661cfd6a63a3a24696feaa9640614f9e7ee329
TrishnaShelly <105371850+TrishnaShelly@users.noreply.github.com>
2022-09-16 22:20:41 +0530

    Reverse of string using recurssion

---
+++ stringReverse.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void reverse(string str)
+{
+    if (str.length() == 0)
+        return;
+    string ros = str.substr(1);
+    reverse(ros);
+    cout << str[0];
+}
+
+int main()
+{
+    string str;
+    getline(cin,str);
+    reverse(str);
+    // cout<<" "<<str;
+    return 0;
+}
