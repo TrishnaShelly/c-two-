254317b2a9932a9812f279d2a5f49c6f3f2fb16d
TrishnaShelly <105371850+TrishnaShelly@users.noreply.github.com>
2022-09-18 22:41:21 +0530

    pairing
    find the number of ways in which n friends can remain single
     or can be paired up.

---
+++ paiaring.cpp
@@ -0,0 +1,18 @@
+// find the number of ways in which n friends can remain single
+// or can be paired up.
+
+#include <iostream>
+using namespace std;
+
+int pairing(int n)
+{
+    if (n == 0 || n == 1 || n == 2)
+        return n;
+    return pairing(n-1)+pairing(n-2)*(n-1);
+}
+
+int main()
+{
+    cout<<pairing(4);
+    return 0;
+}
